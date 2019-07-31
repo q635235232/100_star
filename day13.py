82f48d2dc5d2290be791873088bfeb8035204d93
q635235232 <794537367@qq.com>
2019-07-31 23:09:49 +0800

    day13

--- day13.py
+++ day13.py
@@ -1,7 +1,10 @@
 from random import randint
 from time import time, sleep
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 from os import getpid
+from threading import Thread, Lock
+import tkinter
+import tkinter.messagebox
 
 
 def download_task(filename):
@@ -40,5 +43,176 @@ def main2():
     print('总共耗费了%.2f秒.' % (end - start))
 
 
+def sub_task(string, q):
+    counter = 0
+    if not q.empty():
+        couter = q.get()
+    while couter < 10:
+        if not q.empty():
+            couter = q.get()
+        print(string, end='', flush=True)
+        couter += 1
+        q.put(couter)
+        sleep(0.01)
+
+
+def main3(q):
+    Process(target=sub_task, args=('Ping', q,)).start()
+    Process(target=sub_task, args=('Pong', q,)).start()
+
+
+# 多线程
+def download(filename):
+    print('开始下载%s...' % filename)
+    time_to_download = randint(5, 10)
+    sleep(time_to_download)
+    print('%s下载完成! 耗费了%d秒' % (filename, time_to_download))
+
+
+def main4():
+    start = time()
+    t1 = Thread(target=download, args=('Python从入门到住院.pdf',))
+    t1.start()
+    t2 = Thread(target=download, args=('Peking Hot.avi',))
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.3f秒' % (end - start))
+
+
+class DownloadTask(Thread):
+    def __init__(self, filename):
+        super().__init__()
+        self._filename = filename
+
+    def run(self):
+        print('开始下载%s...' % self._filename)
+        time_to_download = randint(5, 10)
+        sleep(time_to_download)
+        print('%s下载完成! 耗费了%d秒' % (self._filename, time_to_download))
+
+
+def main5():
+    start = time()
+    t1 = DownloadTask('Python从入门到住院.pdf')
+    t1.start()
+    t2 = DownloadTask('Peking Hot.avi')
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.2f秒.' % (end - start))
+
+
+# 没锁线程
+class Account(object):
+
+    def __init__(self):
+        self._balance = 0
+
+    def deposit(self, money):
+        # 计算存款后的余额
+        new_balance = self._balance + money
+        # 模拟受理存款业务需要0.01秒的时间
+        sleep(0.01)
+        # 修改账户余额
+        self._balance = new_balance
+
+    @property
+    def balance(self):
+        return self._balance
+
+
+class AddMoneyThread(Thread):
+
+    def __init__(self, account, money):
+        super().__init__()
+        self._account = account
+        self._money = money
+
+    def run(self):
+        self._account.deposit(self._money)
+
+
+def main6():
+    account = Account()
+    threads = []
+    # 创建100个存款的线程向同一个账户中存钱
+    for _ in range(100):
+        t = AddMoneyThread(account, 1)
+        threads.append(t)
+        t.start()
+    # 等所有存款的线程都执行完毕
+    for t in threads:
+        t.join()
+    print('账户余额为: ￥%d元' % account.balance)
+
+
+class Accout1(object):
+    def __init__(self):
+        self._balance = 0
+        self._lock = Lock()
+
+    def deposit(self, money):
+        self._lock.acquire()
+        try:
+            new_balance = self._balance + money
+            sleep(0.01)
+            self._balance = new_balance
+        finally:
+            self._lock.release()
+
+    @property
+    def balance(self):
+        return self._balance
+
+
+class AddMoneyThread1(Thread):
+
+    def __init__(self, account, money):
+        super().__init__()
+        self._account = account
+        self._money = money
+
+    def run(self):
+        self._account.deposit(self._money)
+
+
+def main7():
+    account = Accout1()
+    threads = []
+    for _ in range(100):
+        t = AddMoneyThread1(account, 1)
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
+    print('账户余额为: ￥%d元' % account.balance)
+
+
+def download1():
+    sleep(10)
+    tkinter.messagebox.showinfo('提示', '下载完成!')
+
+
+def show_about():
+    tkinter.messagebox.showinfo('关于', '作者: 骆昊(v1.0)')
+
+
+def main8():
+    top = tkinter.Tk()
+    top.title('单线程')
+    top.geometry('200x150')
+    top.wm_attributes('-topmost', True)
+
+    panel = tkinter.Frame(top)
+    button1 = tkinter.Button(panel, text='下载', command=download1)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='关于', command=show_about)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
+    tkinter.mainloop()
+
 if __name__ == '__main__':
-    main2()
+    main8()
