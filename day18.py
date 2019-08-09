2572f610bb75e5ce4c4e4e5ddbdd013c8fe76d36
q635235232 <2305774704qq.com>
2019-08-09 17:42:15 +0800

    day18

--- day18.py
+++ day18.py
@@ -32,5 +32,49 @@ def _use_quick_sort():
     print(quick_sort(list1))
 
 
+"""
+    递归回溯法：叫称为试探法，按选优条件向前搜索，当搜索到某一步，发现原先选择并不优或达不到目标时，就退回一步重新选择，比较经典的问题包括骑士巡逻、八皇后和迷宫寻路等。   
+"""
+import sys
+import time
+
+
+def _use_patrol():
+    SIZE = 5
+    total = 0
+
+    def print_board(board):
+        for row in board:
+            for col in row:
+                print(str(col).center(4), end='')
+            print()
+
+    def patrol(board, row, col, step=1):
+        if row >= 0 and row < SIZE and \
+                col >= 0 and col < SIZE and \
+                board[row][col] == 0:
+            board[row][col] = step
+            if step == SIZE * SIZE:
+                nonlocal total
+                total += 1
+                print(f'第{total}种走法: ')
+                print_board(board)
+            patrol(board, row - 2, col - 1, step + 1)
+            patrol(board, row - 1, col - 2, step + 1)
+            patrol(board, row + 1, col - 2, step + 1)
+            patrol(board, row + 2, col - 1, step + 1)
+            patrol(board, row + 2, col + 1, step + 1)
+            patrol(board, row + 1, col + 2, step + 1)
+            patrol(board, row - 1, col + 2, step + 1)
+            patrol(board, row - 2, col + 1, step + 1)
+            board[row][col] = 0
+
+    def use_patrol():
+        board = [[0] * SIZE for _ in range(SIZE)]
+        patrol(board, SIZE - 1, SIZE - 1)
+
+    use_patrol()
+
+
 if __name__ == '__main__':
-    _use_quick_sort()
+    _use_patrol()
