a0482bf68e458ba3246747fece90e7ebe80ad9c5
q635235232 <2305774704qq.com>
2019-08-19 17:02:53 +0800

    day18工厂模式

--- day18.py
+++ day18.py
@@ -121,7 +121,68 @@ def a_function_requiring_decoration():
           "remove my foul smell")
 
 
+"""
+     月薪结算系统 - 部门经理每月15000 程序员每小时200 销售员1800底薪加销售额5%提成
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Emoploee(metaclass=ABCMeta):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def get_salary(self):
+        pass
+
+
+class Manager(Emoploee):
+    def get_salary(self):
+        return 15000.0
+
+
+class Programmer(Emoploee):
+    def __init__(self, name, working_hour=0):
+        self.working_hour = working_hour
+        super().__init__(name)
+
+    def get_salary(self):
+        return 200.0 * self.working_hour
+
+
+class Salesman(Emoploee):
+    def __init__(self, name, sales=0.0):
+        self.sales = sales
+        super().__init__(name)
+
+    def get_salary(self):
+        return 1800.0 + self.sales * 0.05
+
+
+class EmployeeFactory():
+    @staticmethod
+    def create(emp_type, *args, **kwargs):
+        emp_type = emp_type.upper()
+        emp = None
+        if emp_type == 'M':
+            emp = Manager(*args, **kwargs)
+        elif emp_type == 'P':
+            emp = Programmer(*args, **kwargs)
+        elif emp_type == 'S':
+            emp = Salesman(*args, **kwargs)
+        return emp
+
+
+def use_factory():
+    emps = [
+        EmployeeFactory.create('M', '曹操'),
+        EmployeeFactory.create('P', '荀彧', 120),
+        EmployeeFactory.create('P', '郭嘉', 85),
+        EmployeeFactory.create('S', '典韦', 123000),
+    ]
+    for emp in emps:
+        print('%s:%.2f元' % (emp.name, emp.get_salary()))
+
+
 if __name__ == '__main__':
-    a_function_requiring_decoration()
-    a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
-    print(a_function_requiring_decoration.__name__)
+    use_factory()
