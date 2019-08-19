775e5a3cace561ca80af9bdeaa1a4afe44df51c4
q635235232 <2305774704qq.com>
2019-08-19 18:00:24 +0800

    day18自定义字典

--- day18.py
+++ day18.py
@@ -273,5 +273,31 @@ def use_poker():
         print(player.cards)
 
 
-if __name__ == '__main__':
+class SetOnceMappingMixin():
+    """自定义混入类"""
+    __slots__ = ()
+
+    def __setitem__(self, key, value):
+        if key in self:
+            raise KeyError(str(key) + 'already set')
+        return super().__setitem__(key, value)
+
+
+class SetOneDict(SetOnceMappingMixin, dict):
+    """自定义字典"""
     pass
+
+
+def use_set_dict():
+    """使用自定义字典"""
+    my_dict = SetOneDict()
+    try:
+        my_dict['username'] = 'jackfrued'
+        my_dict['username'] = 'hellokitty'
+    except KeyError:
+        pass
+    print(my_dict)
+
+
+if __name__ == '__main__':
+    use_set_dict()
