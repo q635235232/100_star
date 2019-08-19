0ded3abed89341509ca8f929407807c2e05edf07
q635235232 <2305774704qq.com>
2019-08-19 17:37:21 +0800

    day18扑克

--- day18.py
+++ day18.py
@@ -184,5 +184,94 @@ def use_factory():
         print('%s:%.2f元' % (emp.name, emp.get_salary()))
 
 
+# unique会检查重复，重复报错
+# enum枚举
+from enum import Enum, unique
+import random
+
+
+@unique
+class Suite(Enum):
+    """花色"""
+    SPADE, HEART, CLUB, DIAMOND = range(4)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class Card():
+    """牌"""
+
+    def __init__(self, suite, face):
+        self.suite = suite
+        self.face = face
+
+    def show(self):
+        suites = ['♠️', '♥️', '♣️', '♦️']
+        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return f'{suites[self.suite.value]}{faces[self.face]}'
+
+    def __str__(self):
+        return self.show()
+
+    def __repr__(self):
+        return self.show()
+
+
+class Poker():
+    """扑克"""
+
+    def __init__(self):
+        self.index = 0
+        self.cards = [Card(suite, face)
+                      for suite in Suite
+                      for face in range(1, 14)]
+
+    def shuffle(self):
+        """洗牌（打乱顺序）"""
+        random.shuffle(self.cards)
+        self.index = 0
+
+    def deal(self):
+        """发牌"""
+        card = self.cards[self.index]
+        self.index += 1
+        return card
+
+    @property
+    def has_more(self):
+        return self.index < len(self.cards)
+
+
+class Player():
+    """玩家"""
+
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+    def get_one(self, card):
+        """摸牌"""
+        self.cards.append(card)
+
+    def sort(self, comp=lambda card: (card.suite, card.face)):
+        """"整理手头的牌"""
+        self.cards.sort(key=comp)
+
+
+def use_poker():
+    """使用扑克牌"""
+    poker = Poker()
+    poker.shuffle()
+    players = [Player('东邪'), Player('西毒'), Player('南帝'), Player('北丐')]
+    while poker.has_more:
+        for player in players:
+            player.get_one(poker.deal())
+    for player in players:
+        player.sort()
+        print(player.name, end=': ')
+        print(player.cards)
+
+
 if __name__ == '__main__':
-    use_factory()
+    use_poker()
