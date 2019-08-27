5f0872cb694c1dae1bc2c245619b0909ec82892c
q635235232 <794537367@qq.com>
2019-08-28 06:39:10 +0800

    新增切图功能

--- day15.py
+++ day15.py
@@ -10,7 +10,8 @@ def main1():
 
 # 剪裁图像
 def main2():
-    image = Image.open('./res/shiyan.png')
+    filename=str(input('请输入文件名'))
+    image = Image.open('./res/'+filename)
     print(image.size)
     rect1 = image.size[0]
     rect2 = image.size[1]
@@ -18,15 +19,15 @@ def main2():
     j = 1
     k = 0
     while i <= rect2:
+        print(k, i)
         rect = 0, k, 750, i
         image.crop(rect).save('./res/' + str(j) + '.png')
         j += 1
         k += 1050
         i += 1050
-        print(i,k)
-        if i>=rect2:
+        if i >= rect2:
             print(i)
-            rect=0,k,750,rect2
+            rect = 0, k, 750, rect2
             image.crop(rect).save('./res/' + str(j) + '.png')
 
 
--- qietu.py
+++ qietu.py
@@ -4,7 +4,8 @@ from PIL import Image
 # 剪裁图像
 def main2():
     filename = str(input('请输入文件名'))
-    image = Image.open('./res/' + filename)
+    print(filename)
+    image = Image.open(filename)
     print(image.size)
     rect1 = image.size[0]
     rect2 = image.size[1]
@@ -14,14 +15,15 @@ def main2():
     while i <= rect2:
         print(k, i)
         rect = 0, k, 750, i
-        image.crop(rect).save('./res/' + str(j) + '.png')
+        image.crop(rect).save(filename.split('.')[0] + str(j) + '.png')
         j += 1
         k += 1050
         i += 1050
         if i >= rect2:
             print(i)
             rect = 0, k, 750, rect2
-            image.crop(rect).save('./res/' + str(j) + '.png')
+            image.crop(rect).save(filename.split('.')[0] + str(j) + '.png')
+
 
 if __name__ == '__main__':
     main2()
